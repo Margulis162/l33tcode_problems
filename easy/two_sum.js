3e5a7597974c1212fc811bfb1da1efb6048c4276
Margulis162 <markmargulis@proton.me>
2023-11-06 17:18:51 -0500

    easy but better solution

--- easy/two_sum.js
+++ easy/two_sum.js
@@ -36,4 +36,17 @@ var twoSum = function(nums, target) {
 };
 
 
+// easy but cleaner and more efficient solution
+
+
+var twoSum = function(nums, target) {
+    for(let i = 0; i<nums.length; i++){
+        for(let j = i+1; j<nums.length; j++){
+            if((target-nums[i]) == nums[j]){
+                return [i,j];
+            }
+        }
+    }
+};
+
 
