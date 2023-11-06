614443a5ce16302745fe788c6b7bbf1e7a512ae5
Margulis162 <markmargulis@proton.me>
2023-11-06 17:34:01 -0500

    easy chatGPT solution

--- easy/two_sum.js
+++ easy/two_sum.js
@@ -49,4 +49,20 @@ var twoSum = function(nums, target) {
     }
 };
 
+// chatGTP solution 
+
+*/
+function twoSum(nums, target) {
+    const numMap = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (numMap.has(complement)) {
+            return [numMap.get(complement), i];
+        }
+        numMap.set(nums[i], i);
+    }
+    return [];
+}
+
+
 
