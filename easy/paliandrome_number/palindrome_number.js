fcd559ad8ff7b4822693b1b9e6871f2b938855e4
Margulis162 <markmargulis@proton.me>
2023-11-07 21:14:29 -0500

    ups, see previous

--- easy/paliandrome_number/palindrome_number.js
+++ easy/paliandrome_number/palindrome_number.js
@@ -3,29 +3,28 @@ let x =121;
 
 
 var isPalindrome = function(x) {
-    console.log(x); 
-    let str = x.toString();
-     let reversed = '';
-     let j = 0;
-    for (let i = str.length - 1; i >= 0; i--) {
-        reversed += str[i];
-    }
+  console.log(x); 
+  let str = x.toString();
+   let reversed = '';
+   let j = 0;
+  for (let i = str.length - 1; i >= 0; i--) {
+      reversed += str[i];
+  }
+  
+  while( j <= str.length -1){
+    if(str[j] === reversed[j] && j === str.length -1){
+      return true
     
-    while( j <= str.length -1){
-      console.log(str[j], reversed[j])
-      if(str[j] === reversed[j]){
-        
+      
+    }else if(str[j] === reversed[j] ){
         j++;
-        console.log(typeof(str.length))
-      }else if(str[j] === reversed[j] && j === str.length -1){
-        console.log('this')
-        return true;
-      }else{return false;}
-      
-      
-         
+    }else{return false;}
+   
     
        
-    }
+  
+     
+  }
 };
+
 isPalindrome(x);
