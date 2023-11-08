9f67dddd06e6b7241a2ddb04e3de4e2ab967952d
Margulis162 <markmargulis@proton.me>
2023-11-07 21:13:41 -0500

    my first dumb but working solution of the problem

--- easy/paliandrome_number/palindrome_number.js
+++ easy/paliandrome_number/palindrome_number.js
@@ -1,24 +1,31 @@
 
-const x =1000021;
+let x =121;
 
 
 var isPalindrome = function(x) {
-    const str = x.toString();
+    console.log(x); 
+    let str = x.toString();
      let reversed = '';
      let j = 0;
     for (let i = str.length - 1; i >= 0; i--) {
         reversed += str[i];
     }
     
-    console.log(reversed);
     while( j <= str.length -1){
-        if( str[j] === reversed[j]){
-            j++;
-            if( j => str.length - 1){return true;}
-            }else{return false;}
+      console.log(str[j], reversed[j])
+      if(str[j] === reversed[j]){
+        
+        j++;
+        console.log(typeof(str.length))
+      }else if(str[j] === reversed[j] && j === str.length -1){
+        console.log('this')
+        return true;
+      }else{return false;}
+      
+      
          
     
        
     }
 };
-isPalindrome();
+isPalindrome(x);
