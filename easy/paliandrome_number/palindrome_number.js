51bf711ea408b63b5b6962a2b500053495ade0b9
Margulis162 <markmargulis@proton.me>
2023-11-08 21:43:01 -0500

    work in progress on poliandrome check no string solution

--- easy/paliandrome_number/palindrome_number.js
+++ easy/paliandrome_number/palindrome_number.js
@@ -1,3 +1,4 @@
+// my first inefficient solution
 
 let x =121;
 
@@ -28,3 +29,50 @@ var isPalindrome = function(x) {
 };
 
 isPalindrome(x);
+
+
+
+// somebody else's pretty cool solution 
+
+var isPalindrome = function(x) {
+  let pali = x.toString()
+  .split('')
+  .reverse()
+  .join('')
+  let normal = x.toString()
+ return (pali === normal)
+};
+
+
+// work in progress on no string solution 
+var isPalindrome = function(x) {
+  if( x < 0)
+  {return false;}
+  else if(x<10){
+    return true;
+  }else{
+    const num = numOfDigits(x);
+    const rev = reversator(x,num);
+    
+    return (x === rev);
+  }
+  };
+  
+  function numOfDigits(x){
+    num = Math.floor(Math.log10(x) + 1);
+    return num;
+  }
+  
+  function reversator(x, num){
+     let pow = 0;
+     let rev = 0;
+     while(num > 1){
+      
+      rev += (Math.floor(x/Math.pow(10,num-1))*Math.pow(10, pow));
+      pow++;
+      num--;
+      
+    }
+    // console.log((11/Math.pow(10,num-1))*Math.pow(10, 2));
+    return rev;
+  }
