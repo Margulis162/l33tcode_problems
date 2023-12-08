cd3263506c68a79c46881a039faf6cede85eaa20
Margulis162 <markmargulis@proton.me>
2023-12-07 21:19:35 -0500

    my initial solution

--- easy/counter/counter.js
+++ easy/counter/counter.js
@@ -1,3 +1,4 @@
+// I'm learning siomethingh here
 function outerFunction(a){
     return function innerFunction(b){
         const sum = a+b;
@@ -7,3 +8,13 @@ function outerFunction(a){
 
 const addTo6 = outerFunction(6);
 console.log(addTo6(1));
+
+// my initial solution 
+
+var createCounter = function(n) {
+    
+    return function() {
+        return n++;
+    };
+};
+
