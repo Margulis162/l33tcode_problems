7d7077e0d7437f69cf52f5c970477b4da87a5be9
Margulis162 <markmargulis@proton.me>
2023-12-15 21:22:37 -0500

    my initial attempet, not finished

---
+++ easy/jump_game/script.js
@@ -0,0 +1,25 @@
+var canJump = function(nums) {
+    let zeroIndeces =[];
+    //that gives me the indeces of all 0s
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] == 0){
+            zeroIndeces.push(i);
+        }
+    }
+    if(zeroIndeces.length > 0){
+       for(let j =0; j < zeroIndeces.length; j++){
+           ///here I need snippet which detects if I can jump over the 0 at this particular index;
+           let stepBack = zeroIndeces[0] - 1;
+           console.log( stepBack, nums[stepBack], nums[stepBack] + stepBack );
+           if(nums[stepBack] + stepBack > zeroIndeces[j]){
+               return true;
+           }else if(stepBack > 0){
+               stepBack--;
+           }else{return false}
+       }
+    }else{
+        return true;
+    }
+  
+    
+};
