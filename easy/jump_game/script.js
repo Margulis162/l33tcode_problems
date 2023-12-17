17e87bfc1e21e5dc857263ddf414c54a91e50a28
Margulis162 <markmargulis@proton.me>
2023-12-16 21:29:22 -0500

    another attempt

--- easy/jump_game/script.js
+++ easy/jump_game/script.js
@@ -9,7 +9,7 @@ var canJump = function(nums) {
     if(zeroIndeces.length > 0){
        for(let j =0; j < zeroIndeces.length; j++){
            ///here I need snippet which detects if I can jump over the 0 at this particular index;
-           let stepBack = zeroIndeces[0] - 1;
+           let stepBack = zeroIndeces[0] - 1; // I'm trying to avoid loop inside a loop, for that I might need to move that variable outside the loop
            console.log( stepBack, nums[stepBack], nums[stepBack] + stepBack );
            if(nums[stepBack] + stepBack > zeroIndeces[j]){
                return true;
